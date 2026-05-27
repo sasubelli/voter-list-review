d4e7b77ae8f1d4eed988d7c7d9be5f560dd98585
chandra Mouli sasubelli <cm.sasubelli@gmail.com>
2026-05-27 16:24:19 +0200

    Delete src/main directory

--- src/main/java/org/example/oops/InterfaceImpl.java
+++
@@ -1,8 +0,0 @@
-package org.example.oops;
-
-public class InterfaceImpl implements BaseInterface {
-    @Override
-    public void display() {
-        System.out.println("Concrete implementation of display()");
-    }
-}
